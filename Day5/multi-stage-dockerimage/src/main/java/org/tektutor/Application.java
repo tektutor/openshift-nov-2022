dc0b8e37542ca83f587ac0f9cd6e60b7fdacfc76
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-11-11 11:25:01 +0530

    Added Day5 lab instructions and sample code.

---
+++ Day5/multi-stage-dockerimage/src/main/java/org/tektutor/Application.java
@@ -0,0 +1,14 @@
+package org.tektutor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main ( String[] args ) {
+        ApplicationContext ctx = SpringApplication.run( Application.class, args );
+    }
+
+}
